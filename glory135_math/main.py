e2414602dffc1380d796b3ba4a06162fdda5da8f
Adeyemi Glory <79275377+Glory135@users.noreply.github.com>
2024-03-24 19:48:27 +0100

    Update main.py

--- glory135_math/main.py
+++ glory135_math/main.py
@@ -6,11 +6,8 @@ def power(x: int, y: int) -> Union[float, int]:
     Calculate the result of raising x to the power of y.
 
     :param x: The base number (int).
-    :type x: int
     :param y: The exponent.
-    :type y: int
     :return: The result of x^y as a float if both x and y are floats, otherwise an int.
-    :rtype: float or int
     """
     res = x**y
     return res
@@ -21,9 +18,7 @@ def factorial(n: int) -> int:
     Calculate the factorial of n.
 
     :param n: A non-negative integer.
-    :type n: int
     :return: The factorial of n
-    rtype: int
     """
     if n < 0:
         raise ValueError("n cannot be a negative number")
@@ -39,11 +34,8 @@ def permutation(n: int, r: int) -> float:
     Calculate the number of ways to choose r items from a set of n items without repetition.
 
     :param n: The total number of items.
-    :type n: int
     :param r: The number of items to choose.
-    :type r: int
     :return: The number of possible combinations.
-    :rtype: float
     """
     if n < r:
         raise ValueError("r should not be greater than n")
@@ -56,11 +48,8 @@ def combination(n: int, r: int) -> float:
     Calculate the number of ways to choose r items from a set of n items with replacement allowed.
 
     :param n: The total number of items.
-    :type n: int
     :param r: Number of items chosen each time.
-    :type r: int
     :return: The number of possible combinations
-    :rtype: float
     """
     res = permutation(n, r) * (1 / factorial(r))
     return res
